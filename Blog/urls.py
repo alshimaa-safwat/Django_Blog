0ba1de634178d85bf49c726c6dc12fef7909b9e1
LSheNNawY <devshennawy@gmail.com>
2021-05-31 16:01:02 +0200

    fix users url

--- Blog/urls.py
+++ Blog/urls.py
@@ -3,5 +3,5 @@ from Blog import views
 
 urlpatterns = [
     path('', views.get_dashboard, name="dashboard.index"),
-    path('', views.get_users, name="dashboard.users.index"),
+    path('users', views.get_users, name="dashboard.users.index"),
 ]
