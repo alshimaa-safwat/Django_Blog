ef755eeadef8b2156bafac3938a534b540bfd6f3
shimaa <shimaa.safwat112@gmail.com>
2021-05-31 13:26:43 -0700

    add get and delete posts

--- Blog/views.py
+++ Blog/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
-from posts.models import Category
-
-
-
-# Create your views here.
-
-# for testing
-# def index(request):
-#     return render(request, 'layouts/dashboard/base.html')
+from posts.models import Category,Post
+from .forms import createCategoryForm
 
 
 def get_categories(request):
@@ -18,46 +11,45 @@ def get_categories(request):
     return render(request, 'admin/content/categories.html', context)
 
 
-# def get_posts(request):
-#     posts = Post.objects.all()
-#     main_content_var = "Posts"
-#     context = {'posts': posts, 'mainContentVar': main_content_var}
-#     return render(request, 'admin/content/postsList.html', context) 
+def add_category(request):
+    if request.method == "POST":
+        category_form = createCategoryForm(request.POST)
+        if category_form.is_valid():
+            category_form.save()
+            return HttpResponseRedirect("/blogs/categories")
+    else:
+        category_form = createCategoryForm()
+        context = {'category_form': category_form}
+        return render(request, 'admin/content/createCategory.html', context) 
+        
+
+def delete_category(request, id):
+    category = Category.objects.get(id=id)
+    category.delete()
+    return HttpResponseRedirect("/blogs/categories")  
 
 
-# def delete_post(request, post_id):
-#     post = Post.objects.get(id=post_id)
-#     post.delete()
-#     return HttpResponseRedirect("/ourBlog/posts")
+def edit_category(request, id):
+    category = Category.objects.get(id=id)
+    if request.method == "POST":
+        category_form = createCategoryForm(request.POST, instance=category)
+        if category_form.is_valid():
+            category_form.save()
+            return HttpResponseRedirect("/blogs/categories")
+    else:
+        category_form = createCategoryForm(instance=category)
+        context = {'category_form': category_form}
+        return render(request, 'admin/content/createCategory.html', context)               
 
 
-# def add_category(request):
-#     if request.method == "POST":
-#         category_form = createCategoryForm(request.POST)
-#         if category_form.is_valid():
-#             category_form.save()
-#             return HttpResponseRedirect("/ourBlog/categories")
-#     else:
-#         category_form = createCategoryForm()
-#         context = {'category_form': category_form}
-#         return render(request, 'admin/content/createCategory.html', context)
+def get_posts(request):
+    posts = Post.objects.all()
+    main_content_var = "Posts"
+    context = {'posts': posts, 'mainContentVar': main_content_var}
+    return render(request, 'admin/content/postsList.html', context) 
 
 
-# def edit_category(request, id):
-#     category = Category.objects.get(id=id)
-#     if request.method == "POST":
-#         category_form = createCategoryForm(request.POST, instance=category)
-#         if category_form.is_valid():
-#             category_form.save()
-#             return HttpResponseRedirect("/ourBlog/categories")
-#     else:
-#         category_form = createCategoryForm(instance=category)
-#         context = {'category_form': category_form}
-#         return render(request, 'admin/content/createCategory.html', context)
-
-
-# def delete_category(request, id):
-#     category = Category.objects.get(id=id)
-#     category.delete()
-#     return HttpResponseRedirect("/ourBlog/categories")       
-
+def delete_post(request, post_id):
+    post = Post.objects.get(id=post_id)
+    post.delete()
+    return HttpResponseRedirect("/blogs/posts")
