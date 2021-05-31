5e331c3794d1a5d7ad0229d3936c865156a74b32
IslamAhmed0 <ea43533@gmail.com>
2021-05-31 09:17:24 +0200

    view

--- Blog/views.py
+++ Blog/views.py
@@ -6,3 +6,45 @@ from django.http import HttpResponse
 #for testing
 def index(request):
     return render(request,'layouts/dashboard/base.html')
+    
+    
+
+    
+def words(request):
+    word = BadWord.objects.all()
+    mainContentVar = "Forbidden Words"
+    context = {'word': word, 'mainContentVar': mainContentVar}
+
+
+
+def deleteWord(request, id):
+    word = BadWord.objects.get(id=id)
+    word.delete()
+
+
+
+def addWord(request):
+    if request.method == "POST":
+        badWord_form = createBadWordForm(request.POST)
+        if badWord_form.is_valid():
+            badWord_form.save()
+
+    else:
+        badWord_form = createBadWordForm()
+        context = {'badWord_form': badWord_form}
+
+
+
+def editWord(request, id):
+    word = BadWord.objects.get(id=id)
+    if request.method == "POST":
+        badWord_form = createBadWordForm(request.POST, instance=word)
+        if badWord_form.is_valid():
+            badWord_form.save()
+
+    else:
+        badWord_form = createBadWordForm(instance=word)
+        context = {'badWord_form': badWord_form}
+
+
+
