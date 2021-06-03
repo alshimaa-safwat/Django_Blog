f290d5fe0d69ba2cc14b28599a0f7595b6df5106
LSheNNawY <devshennawy@gmail.com>
2021-06-03 23:03:03 +0200

    fixing conflicts

--- Blog/urls.py
+++ Blog/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('categories/', views.get_categories),
     path('categories/edit/<id>', views.edit_category),
     path('categories/delete/<id>', views.delete_category),
-    path('categories/new/', views.add_category),
+    path('categories/new', views.add_category),
     path('posts/', views.get_posts),
     path('posts/new/', views.add_post),
     path('delete/<post_id>', views.delete_post),
