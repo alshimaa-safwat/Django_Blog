42d47c92381cd751293e01a6e86e81b3cd685f2f
RihamWalid96 <rihamwalid2@gmail.com>
2021-06-03 20:20:20 -0700

    add model tany

--- posts/models.py
+++ posts/models.py
@@ -53,3 +53,26 @@ class Reaction(models.Model):
 
     def __str__(self):
         return '%s %s %s' % (self.post, self.user, self.react)
+
+class Subscribe(models.Model):
+	user =models.ForeignKey(User, on_delete=models.DO_NOTHING, default=1)
+	category =models.ForeignKey(Category, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return '%s %s' % (self.user, self.category)
+
+class Reply(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+	comment = models.ForeignKey(Comments,on_delete=models.CASCADE)
+	content = models.CharField(max_length=200)
+	date=models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return '%s %s' % (self.user, self.comment)
+
+
+class ForrbedinWord(models.Model):
+	word =models.CharField(max_length=10)
+
+	def __str__(self):
+		return self.word
