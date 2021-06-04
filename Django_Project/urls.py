3d5916f02edcddc095993b500944be1be3f6c06d
IslamAhmed0 <ea43533@gmail.com>
2021-06-04 22:08:49 +0200

    tag

--- Django_Project/urls.py
+++ Django_Project/urls.py
@@ -14,9 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
+from posts import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('dashboard/',include('Blog.urls'))
+    path('', views.home_page),
+    path('dashboard/', include('Blog.urls')),
+    path('posts/', include('posts.urls')),
+    path('register/',user_views.register,name='register'),
+    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
+
 ]
