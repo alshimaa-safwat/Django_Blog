2902d76a6f3bca41d0083a0ab39996066ab1454a
Muhammed Shendi <39719317+mashendi@users.noreply.github.com>
2021-06-05 00:41:12 +0200

    fixed register

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm,UserLoginForm
+from .forms import UserRegisterForm, UserLoginForm
+from django.contrib import auth
 
 
 def register(request):
@@ -10,15 +12,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You are now able to log in')
+            messages.success(
+                request, f'Your account has been created! You are now able to log in')
             return redirect('login')
     else:
-        form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
-
-
-# @login_required
-# def home(request):
-#     return HttpResponseRedirect(
-#                reverse(NAME_OF_PROFILE_VIEW, 
-#                        args=[request.user.username]))
+        if request.user.is_authenticated:
+            return HttpResponseRedirect('/')
+        else:
+            form = UserRegisterForm()
+            return render(request, 'users/register.html', {'form': form})
